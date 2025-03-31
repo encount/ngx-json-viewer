f0478b3d3ccdf9a289c147bf7594b088e2af0e7d
Martin Thorsen Ranang <mtr@ranang.org>
2025-04-01 01:25:14 +0200

    Refactor to make NgxJsonViewerComponent modern, Angular 15.2+, standalone component.
    Removed NgxJsonViewerModule and made NgxJsonViewerComponent standalone by adding necessary imports directly to the component. Updated relevant exports and dependencies to support the new structure.

--- src/ngx-json-viewer/ngx-json-viewer.component.ts
+++ src/ngx-json-viewer/ngx-json-viewer.component.ts
@@ -1,152 +1,163 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
 
 export interface Segment {
-  key: string;
-  value: any;
-  type: undefined | string;
-  description: string;
-  expanded: boolean;
+    key: string;
+    value: any;
+    type: undefined | string;
+    description: string;
+    expanded: boolean;
 }
 
 @Component({
-  selector: 'ngx-json-viewer',
-  templateUrl: './ngx-json-viewer.component.html',
-  styleUrls: ['./ngx-json-viewer.component.scss']
+    selector: 'ngx-json-viewer',
+    templateUrl: './ngx-json-viewer.component.html',
+    styleUrls: ['./ngx-json-viewer.component.scss'],
+    standalone: true,
+    imports: [
+        NgForOf,
+        NgClass,
+        NgIf
+    ]
 })
 export class NgxJsonViewerComponent implements OnChanges {
 
-  @Input() json: any;
-  @Input() expanded = true;
-  @Input() depth = -1;
+    @Input() json: any;
+    @Input() expanded = true;
+    @Input() depth = -1;
 
-  @Input() _currentDepth = 0;
+    @Input() _currentDepth = 0;
 
-  segments: Segment[] = [];
+    segments: Segment[] = [];
 
-  ngOnChanges() {
-    this.segments = [];
+    ngOnChanges() {
+        this.segments = [];
 
-    // remove cycles
-    this.json = this.decycle(this.json);
+        // remove cycles
+        this.json = this.decycle(this.json);
 
-    if (typeof this.json === 'object') {
-      Object.keys(this.json).forEach(key => {
-        this.segments.push(this.parseKeyValue(key, this.json[key]));
-      });
-    } else {
-      this.segments.push(this.parseKeyValue(`(${typeof this.json})`, this.json));
-    }
-  }
-
-  isExpandable(segment: Segment) {
-    return segment.type === 'object' || segment.type === 'array';
-  }
-
-  toggle(segment: Segment) {
-    if (this.isExpandable(segment)) {
-      segment.expanded = !segment.expanded;
-    }
-  }
-
-  private parseKeyValue(key: any, value: any): Segment {
-    const segment: Segment = {
-      key: key,
-      value: value,
-      type: undefined,
-      description: '' + value,
-      expanded: this.isExpanded()
-    };
-
-    switch (typeof segment.value) {
-      case 'number': {
-        segment.type = 'number';
-        break;
-      }
-      case 'boolean': {
-        segment.type = 'boolean';
-        break;
-      }
-      case 'function': {
-        segment.type = 'function';
-        break;
-      }
-      case 'string': {
-        segment.type = 'string';
-        segment.description = '"' + segment.value + '"';
-        break;
-      }
-      case 'undefined': {
-        segment.type = 'undefined';
-        segment.description = 'undefined';
-        break;
-      }
-      case 'object': {
-        // yea, null is object
-        if (segment.value === null) {
-          segment.type = 'null';
-          segment.description = 'null';
-        } else if (Array.isArray(segment.value)) {
-          segment.type = 'array';
-          segment.description = 'Array[' + segment.value.length + '] ' + JSON.stringify(segment.value);
-        } else if (segment.value instanceof Date) {
-          segment.type = 'date';
+        if (typeof this.json === 'object') {
+            Object.keys(this.json).forEach(key => {
+                this.segments.push(this.parseKeyValue(key, this.json[key]));
+            });
         } else {
-          segment.type = 'object';
-          segment.description = 'Object ' + JSON.stringify(segment.value);
+            this.segments.push(this.parseKeyValue(
+                `(${typeof this.json})`,
+                this.json
+            ));
         }
-        break;
-      }
     }
 
-    return segment;
-  }
+    isExpandable(segment: Segment) {
+        return segment.type === 'object' || segment.type === 'array';
+    }
 
-  private isExpanded(): boolean {
-    return (
-      this.expanded &&
-      !(this.depth > -1 && this._currentDepth >= this.depth)
-    );
-  }
-
-  // https://github.com/douglascrockford/JSON-js/blob/master/cycle.js
-  private decycle(object: any) {
-    const objects = new WeakMap();
-    return (function derez(value, path) {
-      let old_path;
-      let nu: any;
-
-      if (
-        typeof value === 'object'
-        && value !== null
-        && !(value instanceof Boolean)
-        && !(value instanceof Date)
-        && !(value instanceof Number)
-        && !(value instanceof RegExp)
-        && !(value instanceof String)
-      ) {
-        old_path = objects.get(value);
-        if (old_path !== undefined) {
-          return {$ref: old_path};
+    toggle(segment: Segment) {
+        if (this.isExpandable(segment)) {
+            segment.expanded = !segment.expanded;
         }
-        objects.set(value, path);
+    }
 
-        if (Array.isArray(value)) {
-          nu = [];
-          value.forEach(function (element, i) {
-            nu[i] = derez(element, path + '[' + i + ']');
-          });
-        } else {
-          nu = {};
-          Object.keys(value).forEach(function (name) {
-            nu[name] = derez(
-              value[name],
-              path + '[' + JSON.stringify(name) + ']'
-            );
-          });
+    private parseKeyValue(key: any, value: any): Segment {
+        const segment: Segment = {
+            key: key,
+            value: value,
+            type: undefined,
+            description: '' + value,
+            expanded: this.isExpanded()
+        };
+
+        switch (typeof segment.value) {
+            case 'number': {
+                segment.type = 'number';
+                break;
+            }
+            case 'boolean': {
+                segment.type = 'boolean';
+                break;
+            }
+            case 'function': {
+                segment.type = 'function';
+                break;
+            }
+            case 'string': {
+                segment.type = 'string';
+                segment.description = '"' + segment.value + '"';
+                break;
+            }
+            case 'undefined': {
+                segment.type = 'undefined';
+                segment.description = 'undefined';
+                break;
+            }
+            case 'object': {
+                // yea, null is object
+                if (segment.value === null) {
+                    segment.type = 'null';
+                    segment.description = 'null';
+                } else if (Array.isArray(segment.value)) {
+                    segment.type = 'array';
+                    segment.description = 'Array[' + segment.value.length + '] ' + JSON.stringify(
+                        segment.value);
+                } else if (segment.value instanceof Date) {
+                    segment.type = 'date';
+                } else {
+                    segment.type = 'object';
+                    segment.description = 'Object ' + JSON.stringify(segment.value);
+                }
+                break;
+            }
         }
-        return nu;
-      }
-      return value;
-    }(object, '$'));
-  }
+
+        return segment;
+    }
+
+    private isExpanded(): boolean {
+        return (
+            this.expanded &&
+            !(this.depth > -1 && this._currentDepth >= this.depth)
+        );
+    }
+
+    // https://github.com/douglascrockford/JSON-js/blob/master/cycle.js
+    private decycle(object: any) {
+        const objects = new WeakMap();
+        return (function derez(value, path) {
+            let old_path;
+            let nu: any;
+
+            if (
+                typeof value === 'object'
+                && value !== null
+                && !(value instanceof Boolean)
+                && !(value instanceof Date)
+                && !(value instanceof Number)
+                && !(value instanceof RegExp)
+                && !(value instanceof String)
+            ) {
+                old_path = objects.get(value);
+                if (old_path !== undefined) {
+                    return {$ref: old_path};
+                }
+                objects.set(value, path);
+
+                if (Array.isArray(value)) {
+                    nu = [];
+                    value.forEach(function (element, i) {
+                        nu[i] = derez(element, path + '[' + i + ']');
+                    });
+                } else {
+                    nu = {};
+                    Object.keys(value).forEach(function (name) {
+                        nu[name] = derez(
+                            value[name],
+                            path + '[' + JSON.stringify(name) + ']'
+                        );
+                    });
+                }
+                return nu;
+            }
+            return value;
+        }(object, '$'));
+    }
 }
